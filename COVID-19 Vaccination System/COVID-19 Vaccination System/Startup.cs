027fd376b0a9f123f09e7d1a5b17b791c96253c1
IvanAndreski <ivan.andreski@gmail.com>
2021-07-12 14:02:47 +0200

    Init

---
+++ COVID-19 Vaccination System/COVID-19 Vaccination System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COVID_19_Vaccination_System.Startup))]
+namespace COVID_19_Vaccination_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
